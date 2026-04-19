6662be1b17cf2bf061e95f6783cee77e2e1251fa
AlbertFlowX16 <abhchow@gmail.com>
2026-04-20 00:28:34 +1000

    fix: remove circular import

--- src/scripts/learning_tracks.lua
+++ src/scripts/learning_tracks.lua
@@ -3,7 +3,7 @@ local pan = dofile(reaper.GetResourcePath().."/Scripts/src/utils/pan.lua")
 local arr_utils = dofile(reaper.GetResourcePath().."/Scripts/src/utils/arr_utils.lua")
 local daw_state = dofile(reaper.GetResourcePath().."/Scripts/src/utils/daw_state.lua")
 local file_io = dofile(reaper.GetResourcePath().."/Scripts/src/utils/file_io.lua")
-local export = dofile(reaper.GetResourcePath().."/Scripts/src/commands/export.lua")
+local make_all_tracks = dofile(reaper.GetResourcePath().."/Scripts/src/commands/make_all_tracks.lua")
 
 function main()
   local n = reaper.GetNumTracks()
@@ -31,12 +31,12 @@ function main()
       vp = true
     end
     reaper.SetMediaTrackInfo_Value(bottom_track, "D_PAN", 0); -- no need to set metronome volume because we never touch it elsewhere
-    export.export_all(n-1, project_name, path, vp, original_volumes)
+    make_all_tracks.export_all(n-1, project_name, path, vp, original_volumes)
   else
     if bottom_track_name == "VP" then
       vp = true
     end
-    export.export_all(n, project_name, path, vp, original_volumes)
+    make_all_tracks.export_all(n, project_name, path, vp, original_volumes)
   end
 
   daw_state.set_pans(original_pans)
